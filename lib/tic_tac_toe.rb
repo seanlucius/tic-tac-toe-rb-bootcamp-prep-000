2d111f8f303331e086a54fd66b8e03baf469d85d
Sean Lucius <seanlucius22@icloud.com>
2018-10-02 20:54:18 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -11,7 +11,7 @@ def input_to_index(user_input)
 end
 
 def move(board, index, current_player)
-  board[index] = current_player(board)
+  board[index] = current_player
 end
 
 def position_taken?(board, location)
