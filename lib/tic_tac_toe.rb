9614d1e8d470500765cc91b0a6936a34765c28b4
Sean Lucius <seanlucius22@icloud.com>
2018-10-02 20:42:54 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -34,3 +34,18 @@ def turn(board)
   end
 end
 
+def turn_count(board)
+  counter = 0 
+  board.each do |space|
+    if space == "X" || space == "O"
+      counter += 1
+    end 
+  end
+  counter
+end 
+
+def current_player(board)
+  turn = turn_count(board)
+  (turn % 2 == 0) ? "X" : "O"
+ 
+end
